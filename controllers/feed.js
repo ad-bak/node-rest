e3a0bb79e8564405dc39a20dc821c66efdbb2425
Adnan <adbak.dev@gmail.com>
2023-01-01 20:30:53 +0100

    added image, modified controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/TESTING.jpg",
+        creator: {
+          name: "Joe Doe",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -9,7 +20,13 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Joe Doe" },
+      createdAt: new Date(),
+    },
   });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const feedController = require('../controllers/feed');
+const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post("/posts", feedController.createPost);
 
 module.exports = router;
