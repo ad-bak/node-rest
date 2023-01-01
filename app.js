7d66f350f984716139385a2d5dd2c481af619336
Adnan <adbak.dev@gmail.com>
2023-01-01 20:48:16 +0100

    installed mongoose

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
 app.listen(8080);
--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { body } = require("express-validator");
+
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
@@ -8,6 +10,13 @@ const router = express.Router();
 router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post("/posts", feedController.createPost);
+router.post(
+  "/posts",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
