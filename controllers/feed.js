150af1251bfdee69a09d2c39bc1bb1113723d677
Adnan <adbak.dev@gmail.com>
2023-01-02 12:04:43 +0100

    fixed image upload

--- controllers/feed.js
+++ controllers/feed.js
@@ -25,13 +25,19 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if (!req.file) {
+    const error = new Error("No image provided.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
     creator: { name: "Joe Doe" },
-    imageUrl: "images/TESTING.jpg",
+    imageUrl: imageUrl,
   });
   post
     .save()
